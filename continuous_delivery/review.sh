d1a7e06e91e145cfb7bcabf669e393e92cd64649
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-01-21 12:43:58 -0300

    Adicionado script para delivery e configuração no servidor de produção

---
+++ continuous_delivery/review.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+: '
+    Este arquivo contém a sequência de comandos para configurar o ambiente da aplicação. Todas as configurações
+    do SO, Servidor, Banco e Aplicação deve ser adicionado aqui para colocar a aplicação em funcionamento. Este
+    arquivo será executado no servidor a partir de uma conexão SSH.
+
+    Atenção! Mude o valor das variáveis abaixo com informações do projeto.
+
+'
+
+PROJECT="GiC"
+DOMAIN="gic.unscode.com"
+GIT_BRANCH="master"
+GIT_REMOTE_SSH="git@gitlab.com:unscode/gic.git"
+SLACK_WEBHOOK="https://hooks.slack.com/services/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"
+MESSAGE="
+{
+    \"attachments\": [
+        {
+            \"pretext\": \"Olá, tem algo novo do projeto *$PROJECT*\",
+            \"color\": \"#36a64f\",
+            \"title\": \"http://$DOMAIN\",
+            \"title_link\": \"http://$DOMAIN\",
+            \"text\": \"Acesse o link acima para verificar e validar as alterações\",
+        }
+    ]
+}
+"
+
+# Verifica se o diretório do projeto existe no diretório padrão do Apache
+# Atenção, as configurações de subdomínio devem estar configurado.
+if [ -d /var/www/$DOMAIN ]; then
+    # Muda para o diretório da aplicação e troca de proprietário para o usuário atual.
+    cd /var/www/$DOMAIN && sudo chown $USER:$USER . -R
+    # Fetch all remotes e reseta o diretório local com o remoto.
+    git fetch --all && git reset --hard origin/$GIT_BRANCH
+    # Remove o diretório de dependêcia do framework e instala novamente.
+    if [ -d vendor ]; then sudo rm vendor -R; fi
+    composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scripts
+    # Faz as configurações do banco de dados e armazenamento.
+    if [ -f storage/database.sqlite ]; then
+        sudo rm storage/database.sqlite
+    fi
+    touch storage/database.sqlite && php artisan migrate --seed && php artisan storage:link
+    # Adiciona o arquivo de variáveis de ambiente do framework e gera a chave.
+    cp .env.review .env && php artisan key:generate
+    # Muda o proprietário dos arquivos e diretórios.
+    sudo chown www-data:www-data . -R
+    curl -X POST -H 'Content-type: application/json' --data "$MESSAGE" $SLACK_WEBHOOK
+    exit
+fi
+
+# Se o diretório não existe ...
+if [ ! -d /var/www/$DOMAIN ]; then
+    # CONFIGURAÇÕES DE SUBDOMÍNIO NO APACHE
+    sudo echo "
+    <VirtualHost *:80>
+        ServerAdmin jorgerodrigues9@outlook.com
+        ServerName ${DOMAIN}
+        ServerAlias www.${DOMAIN}
+        DocumentRoot /var/www/${DOMAIN}/public
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        <Directory /var/www/${DOMAIN}/public>
+            Allowoverride All
+        </Directory>
+    </VirtualHost>" | sudo tee /etc/apache2/sites-available/$DOMAIN.conf
+    sudo a2ensite $DOMAIN
+    sudo /etc/init.d/apache2 restart
+    # Vai para o diretório de arquivos do servidor, cria um diretória para o site, e muda as permissões de usuário.
+    # Importante, o nome do diretório deve ser o domnínio sem o 'www'
+    cd /var/www/ && sudo mkdir $DOMAIN && sudo chown $USER:$USER $DOMAIN -R
+    git clone -b $GIT_BRANCH $GIT_REMOTE_SSH $DOMAIN && cd $DOMAIN
+    # Instala as dependências da aplicação
+    composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scripts && npm install
+    # Configura o banco de dados e armazenameto
+    touch storage/database.sqlite && php artisan migrate --seed && php artisan storage:link
+    # Adiciona o arquivo de variáveis de ambiente do framework e gera a chave.
+    cp .env.review .env && php artisan key:generate
+    sudo chown www-data:www-data . -R
+    curl -X POST -H 'Content-type: application/json' --data "$MESSAGE" $SLACK_WEBHOOK
+    exit
+fi
