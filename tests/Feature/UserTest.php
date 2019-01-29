dd972f41a86dab6eba5f7932a83a6188b1ae78d7
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-01-28 21:57:09 -0300

    Atualizado testes para usuário

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,12 +3,29 @@
 namespace Tests\Feature;
 
 use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
-class UserTest
+class UserTest extends TestCase
 {
     public function test_user_can_see_user() // phpcs:disable
     {
-        $response = $this->get('/userr');
-        $response->assertStatus(201);
+        $response = $this->json('GET', '/user');
+        $response->assertResponseStatus(200);
+        $response->seeJsonStructure([
+            "total",
+            "per_page",
+            "current_page",
+            "last_page",
+            "first_page_url",
+            "last_page_url",
+            "next_page_url",
+            "prev_page_url",
+            "path",
+            "from",
+            "to",
+            "data",
+        ]);
     }
 }
