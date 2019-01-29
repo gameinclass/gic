2cee41bba104909ada206d95d06cd8d3acf9c25c
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-01-28 22:15:56 -0300

    Adicionado teste de integração para ususário e ator

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Actor;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -9,7 +10,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class UserTest extends TestCase
 {
-    public function test_user_can_see_user() // phpcs:disable
+    public function test_user_can_index_users() // phpcs:disable
     {
         $response = $this->json('GET', '/user');
         $response->assertResponseStatus(200);
@@ -26,6 +27,19 @@ class UserTest extends TestCase
             "from",
             "to",
             "data",
+        ]);
+    }
+
+    public function test_user_can_create_user() // phpcs:disable
+    {
+        $user = factory(User::class)->create();
+        $data = $user->toArray();
+        $actor = factory(Actor::class)->create(["user_id" => $user->id]);
+        $data["actor"] = $actor->toArray();
+        $response = $this->json('POST', '/user', $data);
+        $response->assertResponseStatus(201);
+        $response->seeJsonEquals([
+            "data" => $data
         ]);
     }
 }
