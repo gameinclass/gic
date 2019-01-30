564622070ca74e564c313f13515e606cc44692fc
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-01-29 21:08:35 -0300

    Adicionado seeder para usuário e ator

---
+++ database/factories/ActorFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\Actor::class, function (Faker $faker) {
+    $boolean = $faker->boolean;
+    return [
+        // 'user_id' => $faker->randomElement(\App\User::pluck('id')->toArray())
+        'is_administrator' => $faker->boolean,
+        'is_design' => $boolean,
+        'is_player' => !$boolean,
+    ];
+});
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class, 10000)->create()->each(function ($user) {
+            $user->actor()->save(factory(\App\Models\Actor::class)->make());
+        });
+    }
+}
