a2311fb8e89defe94518a562dafb0cde167ece3d
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-02-07 18:32:26 -0300

    Adicionado recurso de api para grupo

--- app/Http/Resources/Game/Game.php
+++ app/Http/Resources/Game/Game.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Game;
 
+use App\Http\Resources\Group\Group as Group;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Game extends JsonResource
@@ -18,6 +19,7 @@ class Game extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
+            'groups' => Group::collection($this->groups),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
