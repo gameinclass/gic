a49aff080a8e48f7a22cd382897c80941a72b0b1
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-02-11 11:26:36 -0300

    Adicionado o policiamento de medalha ao provedor de serviço

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Game;
+use App\Models\Medal;
 use App\Models\User;
 use App\Policies\GamePolicy;
+use App\Policies\MedalPolicy;
 use App\Policies\UserPolicy;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
@@ -20,6 +22,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         User::class => UserPolicy::class,
         Game::class => GamePolicy::class,
+        Medal::class => MedalPolicy::class,
     ];
 
     /**
