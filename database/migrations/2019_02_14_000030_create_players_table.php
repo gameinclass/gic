711be15aaeaa33869597f70468a5f32875fd3409
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-02-14 10:06:46 -0300

    Atualizado migrações de grupos e jogadores

--- database/migrations/2019_02_14_000030_create_players_table.php
+++ database/migrations/2019_02_14_000030_create_players_table.php
@@ -18,8 +18,10 @@ class CreatePlayersTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned()->comment("Chave estrangeira para usuário");
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('game_id')->unsigned()->comment("Chave estrangeira para jogos");
+            $table->integer('game_id')->unsigned()->nullable()->comment("Chave estrangeira para jogos");
             $table->foreign('game_id')->references('id')->on('games');
+            $table->integer('phase_id')->unsigned()->comment("Chave estrangeira para fases");
+            $table->foreign('phase_id')->references('id')->on('phases');
             $table->integer('group_id')->unsigned()->nullable()->comment("Chave estrangeira para grupos");
             $table->foreign('group_id')->references('id')->on('groups');
             $table->timestamps();
