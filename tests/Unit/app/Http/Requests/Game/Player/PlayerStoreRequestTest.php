ecd5a39ad6ac6f07fa59629a04199c8e76a85f20
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-02-27 22:21:14 -0400

    Adicionado teste para requisição de jogadores do jogo

---
+++ tests/Unit/app/Http/Requests/Game/Player/PlayerStoreRequestTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\app\Http\Requests\Game\Player;
+
+use Tests\TestCase;
+use App\Http\Requests\Game\Player\PlayerStoreRequest;
+
+class PlayerStoreRequestTest extends TestCase
+{
+    /**
+     * Testa se o usuário está autorizado a fazer a requisição.
+     *
+     * @return void
+     */
+    public function test_authorize()
+    {
+        $playerStoreRequest = new PlayerStoreRequest();
+        $this->assertTrue($playerStoreRequest->authorize());
+    }
+
+    /**
+     * Testa as regras de validação que será aplicado a requisição
+     *
+     * @return void
+     */
+    public function test_rules()
+    {
+        $rules = [
+            'user_id' => 'required|integer',
+        ];
+        $plauerStoreRequest = new PlayerStoreRequest();
+        $this->assertEquals($rules, $plauerStoreRequest->rules());
+    }
+}
