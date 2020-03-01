d3bac8ab11e9d0fe6356b9f58eaaa776efe9c210
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2020-03-01 13:19:48 -0400

    Atualizado desing da página de perfil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,9 +2,21 @@
 
 @section('content')
     <div class="container">
-        <h1 class="">{{Auth::user()->name}}</h1>
-        <h3 class="mb-5">{{Auth::user()->email}}</h3>
+        <h1 class="mt-5">
+            {{Auth::user()->name}}
+        </h1>
+        <h4 class="mb-5">
+            {{Auth::user()->email}}
 
+            @if(Auth::user()->actor)
+                @if(Auth::user()->actor->is_administrator)
+                    <span class="badge badge-warning">Administrador</span>
+                @endif
+                @if(Auth::user()->actor->is_design)
+                    <span class="badge badge-warning">Professor</span>
+                @endif
+            @endif
+        </h4>
         <div class="border border-white rounded">
             <passport-authorized-clients></passport-authorized-clients>
         </div>
