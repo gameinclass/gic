0b37fda21bbeeb40946052d45ddf102eb75faf66
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2020-03-22 08:07:01 -0400

    Atualizado pacotes

---
+++ app/Events/Medal/MedalUpdating.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Events\Medal;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class MedalUpdating
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
